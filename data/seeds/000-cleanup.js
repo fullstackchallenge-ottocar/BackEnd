74b6d07e2c2dfee4372858a7e7c8e3673f0c0033
novinary <novina.dev@gmail.com>
2019-08-15 00:34:31 +0100

    Ran seeding scripts for users and cars

--- data/seeds/000-cleanup.js
+++ data/seeds/000-cleanup.js
@@ -1,13 +1,7 @@
 
+// ./seeds/00-cleanup.js
+const cleaner = require('knex-cleaner');
+
 exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+  return cleaner.clean(knex); // cleans all tables and resets the primary keys
 };
