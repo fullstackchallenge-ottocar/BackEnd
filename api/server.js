7cc056a231b74389c669e4ce35e0e4dc53c8c470
novinary <novina.dev@gmail.com>
2019-08-19 10:21:28 +0100

    Created endpoints for register and login (used Postman for testing)

--- api/server.js
+++ api/server.js
@@ -1,13 +1,17 @@
 const express = require('express');
 
 const configureMiddleware = require('./middleware.js');
+const authRouter = require('../routes/authRoutes.js');
 
 const server = express();
 configureMiddleware(server);
 
+server.use('/api/auth', authRouter);
+
 // sanity check route
-server.get('/', (res) => {
+/*server.get('/', (req,res) => {
     res.send("This is working.");
 });
+*/
 
 module.exports = server;
