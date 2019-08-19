ae9ce2c980bdb8aea2489ac833aedff4e05827cb
novinary <novina.dev@gmail.com>
2019-08-19 14:24:57 +0100

    Created endpoints for auth, cars and stats(used Postman for testing)

--- api/server.js
+++ api/server.js
@@ -3,12 +3,16 @@ const express = require('express');
 const configureMiddleware = require('./middleware.js');
 const authRouter = require('../routes/authRoutes.js');
 const userRouter = require('../routes/userRoutes.js');
+const carRouter = require('../routes/carRoutes.js');
+const statRouter = require('../routes/statRoutes.js');
 
 const server = express();
 configureMiddleware(server);
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', userRouter);
+server.use('/api/cars', carRouter);
+server.use('/api/stats', statRouter);
 
 // sanity check route
 /*server.get('/', (req,res) => {
